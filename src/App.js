668b767d77e26946a47b9c28157b18fe101ac682
KianIsNotAvailable <kianxw@gmail.com>
2023-10-22 17:37:32 +0100

    scrolling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import CSS from './assets/css3.png';
-import Git from './assets/git.png';
-import Github from './assets/github.png';
-import HTML from './assets/html5.png';
-import Javascript from './assets/javascript.png';
-import mySQL from './assets/mysql.png';
-import Nodejs from './assets/nodejs.png';
-import ReactIcon from './assets/react.png';
-import VScode from './assets/visual-studio-code.png';
-import Tailwind from './assets/Tailwind CSS.png'
 import Skills from './components/skillList';
 
 
@@ -19,20 +9,21 @@ function App() {
   return (
     <div className="App">
         <Navbar />
-        <div className=' flex flex-col items-center justify-center h-screen'>
-        <h1 className='font-bold text-3xl'>Hi, I'm Kian</h1>
+        <div id='home' className=' flex flex-col items-center justify-center h-screen'>
+        <h1 className='font-bold text-4xl'>Hi, I'm Kian</h1>
         <h2 className='font-semibold m-5'>Fullstack Developer</h2>
-        <p>I'm a fullstack developer with a passion for all things technology</p>
+        <p className='max-w-3xl'>I'm a fullstack developer with a passion for all things technology. I'm an advocate for clean code, responsive design and continuous improvement. 
+          When I'm not coding you can find me exploring the latest tech innovations and keeping up with the latest trends and best practices to deliver top notch results.</p>
         </div>
-        <section className='skills h-screen'>
+        <section id='SkillSection' className=' h-screen'>
         <h1 className='font-bold text-3xl my-10'>Skills</h1>
           <div className='icons flex flex-row justify-center my-44'>
             <Skills/>
           </div>
         </section>
-        <section className='skills h-screen'>
-          <h1>Projects</h1>
-        <div className='flex flex-row '>
+        <section className='Projects h-screen'>
+          <h1 className='font-bold text-3xl'>Projects</h1>
+        <div className='flex flex-row justify-center p-10 space-x-40'>
           <h2 className=''>Calorie Calculator & Workout builder</h2>
           <h2>Twitch Chat Bot</h2>
           <h2>Pizza Planet Ecommerce Website</h2>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import {BrowserRouter} from "react-router-dom";
+import { Link } from 'react-scroll';
 export default function Navbar() {
+  const [clicked, setClick] = useState(false);
+  const handleClick = () => setClick(!clicked);
+  const closeMenu = () => setClick(false)
   return (
-    <div className='flex flex-row justify-center space-x-20 bg-black text-white h-16 items-center'>
-      <h1 className='cursor-pointer'>Home</h1>
-      <h1 className='cursor-pointer'>Skills</h1>
-      <h1 className='cursor-pointer'>Projects</h1>
-      <h1 className='cursor-pointer'>CV</h1>
+    
+    <div className='flex flex-row fixed w-full justify-center space-x-20 bg-black text-white h-16 items-center'>
+      <BrowserRouter>
+            <nav>
+                <ul className='flex flex-row justify-center space-x-20 items-center'>
+                    <li className='cursor-pointer'><Link to='home' spy={true} smooth={true} offset={-70} duration={500} onClick={closeMenu}>Home</Link></li>
+                    <li className='cursor-pointer'><Link to='SkillSection' spy={true} smooth={true} offset={-70} duration={500}>Skills</Link></li>
+                    <li className='cursor-pointer'><Link to='Projects' spy={true} smooth={true} offset={-70} duration={500}>Projects</Link></li>
+                    <li className='cursor-pointer'><Link to='/CV'>CV</Link></li>
+                </ul>
+            </nav>
+        </BrowserRouter>
     </div>
   )
 }
