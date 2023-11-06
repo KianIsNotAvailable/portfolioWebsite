09eec79ff2832d0d5894e7c2aab2152b0ec084b4
KianIsNotAvailable <kianxw@gmail.com>
2023-11-06 01:49:49 +0000

    change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,7 @@ export default function Projects() {
         <img class="rounded-t-lg p-24" src={chatbot} alt="" />
     </a>
     <div class="p-5">
-        <a href="#">
+        <a href="https://github.com/KianIsNotAvailable/slots-bot">
             <h5 class="mb-2 m-auto text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Twitch Chat Bot</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">An interactive twitch chat bot built with nodeJS and mySQL.</p>
